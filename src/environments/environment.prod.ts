fd30cebc68f02be7892f0cc6951edb9fe7649d9c
gsosam99 <sosag@Guillermo-Sosa.local>
2020-11-13 19:22:54 -0400

    Bootstrap y Firebase instalado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyC625Z3L5QVoP5iFxihMo5TlGomXUNzjQM",
+    authDomain: "sugar-world-project.firebaseapp.com",
+    databaseURL: "https://sugar-world-project.firebaseio.com",
+    projectId: "sugar-world-project",
+    storageBucket: "sugar-world-project.appspot.com",
+    messagingSenderId: "343822246893",
+    appId: "1:343822246893:web:1a95521a800a75da1fbde4",
+    measurementId: "G-3CG512G7L6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyC625Z3L5QVoP5iFxihMo5TlGomXUNzjQM",
+    authDomain: "sugar-world-project.firebaseapp.com",
+    databaseURL: "https://sugar-world-project.firebaseio.com",
+    projectId: "sugar-world-project",
+    storageBucket: "sugar-world-project.appspot.com",
+    messagingSenderId: "343822246893",
+    appId: "1:343822246893:web:1a95521a800a75da1fbde4",
+    measurementId: "G-3CG512G7L6"
+  }
 };
 
 /*
